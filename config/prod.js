af41aad5d4f81a5c642b0092e389dbd02085c175
Dor <dorhakim100@gmail.com>
2024-08-06 12:00:49 +0300

    Connect atlas

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,4 @@
 export default {
-  dbURL:
-    'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  dbURL: 'mongodb+srv://dorhakim100:631da031@gamestore.ywtldex.mongodb.net/',
   dbName: 'GameStore',
 }
--- server.js
+++ server.js
@@ -2,42 +2,40 @@ import express from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import path, { dirname } from 'path'
+import http from 'http'
+
 import { fileURLToPath } from 'url'
 import { MongoClient } from 'mongodb'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 
 import { logger } from './services/logger.service.js'
 logger.info('server.js loaded...')
 
 const app = express()
+const server = http.createServer(app)
 
-// Express App Config
 app.use(cookieParser())
 app.use(express.json())
-app.use(express.static('public'))
 
 if (process.env.NODE_ENV === 'production') {
-  // Express serve static files on production environment
-  app.use(express.static(path.resolve(__dirname, 'public')))
-  console.log('__dirname: ', __dirname)
+  app.use(express.static(path.resolve('public')))
 } else {
-  // Configuring CORS
-  // Make sure origin contains the url
-  // your frontend dev-server is running on
   const corsOptions = {
     origin: [
-      'http://127.0.0.1:5173',
-      'http://localhost:5173',
-
       'http://127.0.0.1:3000',
       'http://localhost:3000',
+      'http://127.0.0.1:5173',
+      'http://localhost:5173',
     ],
     credentials: true,
   }
   app.use(cors(corsOptions))
 }
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+// Express App Config
+// app.use(express.static('public'))
 
 import { authRoutes } from './api/auth/auth.routes.js'
 import { userRoutes } from './api/user/user.routes.js'
